3b870fcff253a5dde43d6d350ec33984159dbd4f
Yshika <31753378+Yshika@users.noreply.github.com>
2017-10-27 23:16:03 +0530

    Update operation.c

--- operation.c
+++ operation.c
@@ -6,12 +6,4 @@ printf("Enter 2 numbers");
 scanf("%d%d",&a,&b);
 sum=a+b;
 printf("Sum of the numbers is:%d",sum);
-}#include<stdio.h>
-void main()
-{
-int a,b,sum;
-printf("Enter 2 numbers");
-scanf("%d%d",&a,&b);
-sum=a+b;
-printf("Sum of the numbers is:%d",sum);
 }
